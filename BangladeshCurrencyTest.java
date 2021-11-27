13fcfd411b9ba806f1dde4063ef4a0ff23ae3154
Zahid Rouf <zahid19@gmail.com>
2021-11-27 16:18:28 +0600

    Create BangladeshCurrencyTest.java

---
+++ BangladeshCurrencyTest.java
@@ -0,0 +1,13 @@
+public class BangladeshCurrencyTest {
+    public static void main(String args[]) {
+    String amount = "150 \u09F3"+" Only" ; 
+    System.out.println(amount);
+    }
+}
+
+/* 
+********************
+Output: 
+150 ৳ Only
+********************
+*/
